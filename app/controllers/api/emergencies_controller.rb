634ab6a657a0ae184f1c1cbf5acd180cf3c6575c
anjankumarhn <aknagaraja@qwinixtech.com>
2016-05-24 20:33:08 +0530

    First test case

---
+++ app/controllers/api/emergencies_controller.rb
@@ -0,0 +1,15 @@
+module Api
+	class EmergenciesController < ApplicationController
+		def new
+			render :json => {:message => "page not found"}.to_json, :status => 404
+		end
+
+		def edit
+			render :json => {:message => "page not found"}.to_json, :status => 404
+		end
+
+		def destroy
+			render :json => {:message => "page not found"}.to_json, :status => 404
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+	get '/emergencies/new' => "api/emergencies#new"
+	get '/emergencies/F-100/edit' => "api/emergencies#edit"
+	delete '/emergencies/F-100' => "api/emergencies#destroy"
 end
